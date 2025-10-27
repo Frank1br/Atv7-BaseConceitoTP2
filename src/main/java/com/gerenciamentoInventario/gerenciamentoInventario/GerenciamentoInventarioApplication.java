8abfbd63ed24f3563c61b49a3e72a2b58bcef877
Frank Oliveira <fm0325836@gmail.com>
2025-10-27 16:23:10 -0300

    feat: Project Structure

---
+++ src/main/java/com/gerenciamentoInventario/gerenciamentoInventario/GerenciamentoInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamentoInventario.gerenciamentoInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciamentoInventario/gerenciamentoInventario/GerenciamentoInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentoInventario.gerenciamentoInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
